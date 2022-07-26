482b3a52ddad8c6aa0f42f721a754b4aeebdca8c
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-07-26 19:58:08 +0300

    Completed styling my side bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,11 @@ import * as FaIcons from "react-icons/fa"
 import * as AiIcons from "react-icons/ai"
 import {Link} from "react-router-dom"
 import SidebarData from "./SidebarData"
-import  "../../src/components/Navbar"
+import { IconContext } from 'react-icons'
+//Allows us to style the icons all at ago
+//=========================================
+
+import  "./Navbar.css"
 
 function Navbar() {
 
@@ -14,21 +18,25 @@ function Navbar() {
     //so we're setting our sidebar to reverse state(to true)
   return (
   <>
+  <IconContext.Provider value={{color: "red"
+  }}>
+
   <div className="navbar">
     <Link to="#" className="menu-bars"><FaIcons.FaBars onClick={showSidebar}/></Link>
   </div>
   <nav className={sidebar? "nav-menu active" : "nav-menu"}>
-    <ul className="nav-menu-items">
+    <ul className="nav-menu-items" onClick={showSidebar}>
         <li className="nav-toggle">
             <Link to='#' className="menu-bars"><AiIcons.AiOutlineClose/></Link>
         </li>
     </ul>
     {SidebarData.map((item,index)=>{
       return (
-        <li key={index} className={item.className}><Link to={item.path}><span>{item.title}</span></Link></li>
+        <li onClick={showSidebar} key={index} className={item.className}><Link to={item.path}>{item.icon}<span>{item.title}</span></Link></li>
       )
     })}
   </nav>
+  </IconContext.Provider>
   </>
   )
 }
