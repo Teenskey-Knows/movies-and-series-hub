47c74807d0b8e2559f3a8216f261771dea207160
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-07-27 12:28:42 +0300

    Almost completing my first component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import pic from "../Images/mypic.jpg"
+import pic2 from "../Images/pic2.jpg"
 
 function Home() {
   return (
@@ -20,6 +22,44 @@ function Home() {
         
       </div>
       <div className="separator"></div>
+      <div className="stayAcquaintedPart">
+        <br />
+        <div className="stayAcquaintedContainer">
+        <img clasName="stayAImage" style={{borderRadius : 30}}src={pic} alt="" height={200} />
+        <div className="stayAcquaintedWordings">
+          <h1 className="h1SAW1">Stay Acquainted</h1>
+          <h2 className="h2SAW">
+            Get to know what shows <br/>
+            people recommend & how <br/>good they are by reviewing their rating
+          </h2>
+        </div>
+        </div>
+  
+      <div className="separator"></div>
+      
+      <div className="activelyContributePart">
+      <br />
+        <div className="activelyContributeContainer">
+          <div className="activelyContributeWordings">
+            <h1 className="ACW1">Actively Contribute.</h1>
+            <h2 className="ACW">
+              Post your favourite <br /> shows as a recommendation <br />  to others.
+            </h2>
+            </div>
+            <div className="ACImage">
+            <img className="AContributeImage" src={pic2} height={250} alt="" />
+
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
+        
+
+      
       
     </>
   );
