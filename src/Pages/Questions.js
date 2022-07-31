95937b8d8099f14cca2ba046a56f866d4507e10d
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-07-31 11:22:11 +0300

    Finalizing on the Review Component

--- src/Pages/Questions.js
+++ src/Pages/Questions.js
@@ -10,19 +10,99 @@ function toggle1(){
 
 const paragraph1 = (<p>This website offrers the visitors a chance to know what shows people recommend. When you visit the movies category from the side bare, you'll see all the movie listings with their description, ratings, genre and when you tap on the image poster, you'll be directed on where to start watching it from.</p>)
 
+
+//===================================
+//STATE 2
+//===================================
+
+const [question2,setQuestion2]=useState(true)
+
+function toggle2(){
+    setQuestion2(!question2)
+}
+
+const paragraph2 = (<p>Yes, as a matter of fact, when you tap on the image poster of the movie or series, you will get re-directed to where you can watch the movie or series.</p>)
+
+//======================================
+//STATE 3
+//======================================
+
+const [question3,setQuestion3]=useState(true)
+
+function toggle3(){
+    setQuestion3(!question3)
+}
+
+const paragraph3 = (<p>Yes, you can post the movie or series that you'd want to be listed on the website. Simply fill up the form on the movies section following all due requirements and it will be posted once you submity your recommendation.
+</p>)
+
+
   return (
    <>
+   <div className="QuestionsContainer">
+    <div className="questionsCard">
    <div className="question1">
     <button onClick={toggle1} className="questionBtn1">
-        <h1 className="myquestion1">
+        
             What is this website about?
-        </h1>
+       
     </button>
-    {question1 ? <p>{paragraph1}</p> : null}
+   
+   </div>
+   
+   <div className="question1Styling">
+   {question1 ? <p>{paragraph1}</p> : null}
+   </div>
    </div>
    
    
+   </div>
+
+
+
+   {/* ==================================== */}
+<br />
+   <div className="QuestionsContainer">
+    <div className="questionsCard">
+   <div className="question1">
+    <button onClick={toggle2} className="questionBtn1">
+        
+            Can one have access to the movies displayed on the website?
+       
+    </button>
+   
+   </div>
+   
+   <div className="question1Styling">
+   {question2 ? <p>{paragraph2}</p> : null}
+   </div>
+   </div>
+   
+   
+   </div>
+{/* ========================== */}
+<br />
+   <div className="QuestionsContainer">
+    <div className="questionsCard">
+   <div className="question1">
+    <button onClick={toggle3} className="questionBtn1">
+        
+            Can I post a movie/series on the website?
+            
+       
+    </button>
+   
+   </div>
+   
+   <div className="question1Styling">
+   {question3 ? <p>{paragraph3}</p> : null}
+   </div>
+   </div>
+   
+   
+   </div>
    </>
+
   )
 }
 
--- src/Pages/Reviews.js
+++ src/Pages/Reviews.js
@@ -1,30 +1,25 @@
-import React from 'react'
-import Questions from './Questions'
-
-
+import React from "react";
+import Questions from "./Questions";
 
 function Reviews() {
- 
   return (
     <>
-   
-    <div className="backgroundIt">
-      <br />
-      <div className="contentbackground">
-      <h1>Frequently Asked Questions</h1>
-      <div className="question1">
-        <h2>What is this site about?<span><button className="one">+</button></span></h2>
-        <Questions/>
-      </div>
+      <div className="Reviewbackground">
+        <div className="ReviewbackgroundCentre">
+          <div className="theContent">
+            <h1 className="myH1">
+              Frequently Asked Questions
+            </h1>
+            <br />
+            <div className="styleQuestionsComponent">
+            <Questions/>
+            </div>
+          </div>
 
+        </div>
       </div>
-
-    </div>
-    
-    
-    
     </>
-  )
+  );
 }
 
-export default Reviews
+export default Reviews;
